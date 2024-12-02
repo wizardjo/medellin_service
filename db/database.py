c6496f3eee182cf373e5645d9419a308173b1a22
JorgeOchoa <>
2024-12-01 23:45:37 -0500

    - Set database

--- db/database.py
+++ db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://admin:antonio@localhost:5432/poker'
+SQLALCHEMY_DATABASE_URL = 'postgresql://wcagrohokh:CLXaG$7MwDwXa3c1@mede-server.postgres.database.azure.com:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
