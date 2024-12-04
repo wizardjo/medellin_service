72a608c02a1419329d2d15a0a3d2f69c684efd73
JorgeOchoa <>
2024-12-03 21:08:35 -0500

    - added docker compose and init.sql

---
+++ init.sql
@@ -0,0 +1,12 @@
+GRANT ALL PRIVILEGES ON DATABASE poker TO admin;
+
+/* CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  age INTEGER
+);
+
+INSERT INTO students (name, age) VALUES
+  ('Mkyong', 40),
+  ('Ali', 28),
+  ('Teoh', 18); */
